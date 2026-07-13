9678ed8ce8ab63bfe6f4ca943377a13770c3901e
vinayagamurthi47bcs28-commits <vinayagamurthi47_bcs28@mepcoeng.ac.in>
2026-07-13 10:28:19 +0530

    Create conting_bits.c

---
+++ conting_bits.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* countBits(int n, int* returnSize) {
+    *returnSize = n + 1;
+    int* ans = (int*)malloc((*returnSize) * sizeof(int));
+    if (ans == NULL) {
+        return NULL;
+    }
+    ans[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        ans[i] = ans[i & (i - 1)] + 1;
+    }
+    return ans;
+}
