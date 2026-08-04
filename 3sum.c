e50dbba806876315ec0e4200665814f3153f57e3
vinayagamurthi47bcs28-commits <vinayagamurthi47_bcs28@mepcoeng.ac.in>
2026-08-04 15:17:55 +0530

    Create 3sum.c

---
+++ 3sum.c
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result= new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            if(nums[i]>0){
+                break;
+            }
+            if(i>0&&nums[i-1]==nums[i]){
+                continue;
+            }
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                int total=nums[i]+nums[left]+nums[right];
+                if(total==0){
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left<right&&nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    while(left<right&&nums[right]==nums[right+1]){
+                        right--;
+                    }
+                }
+                else if(total<0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return result;
+
+    }
+}
+ 
