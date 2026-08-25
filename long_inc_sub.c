410a40a96efbf80acaac1f1c9b2eadf555fe91f9
vinayagamurthi47bcs28-commits <vinayagamurthi47_bcs28@mepcoeng.ac.in>
2026-08-25 16:22:53 +0530

    Update long_inc_sub.c

--- long_inc_sub.c
+++ long_inc_sub.c
@@ -4,14 +4,10 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
         
-        // tails[i] stores the smallest tail of all increasing subsequences of length i + 1
         int[] tails = new int[nums.length];
-        int size = 0; // Tracks the length of the longest increasing subsequence
-        
+        int size = 0; 
         for (int x : nums) {
             int i = 0, j = size;
-            
-            // Binary search to find the correct insertion index for x in tails
             while (i < j) {
                 int mid = i + (j - i) / 2;
                 if (tails[mid] < x) {
@@ -20,16 +16,11 @@ class Solution {
                     j = mid;
                 }
             }
-            
-            // Update the element at the found position
             tails[i] = x;
-            
-            // If x is larger than all elements in tails, it extends the active length
             if (i == size) {
                 size++;
             }
-        }
-        
+        }        
         return size;
     }
 }
