be3d30e74c049df385ad383707bd1eef42440ab9
vinayagamurthi47bcs28-commits <vinayagamurthi47_bcs28@mepcoeng.ac.in>
2026-08-25 16:10:41 +0530

    Update gen_par.py

--- gen_par.py
+++ gen_par.py
@@ -1,17 +1,13 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         def valid_parentheses(n, open_count, curr, res):
-
             if len(curr) == 2 * n:
                 res.append(curr)
                 return
-
             if open_count < n:
                 valid_parentheses(n, open_count + 1, curr + '(', res)
-
             if len(curr) - open_count < open_count:
                 valid_parentheses(n, open_count, curr + ')', res)
-
         result = []
         valid_parentheses(n, 0, '', result)
         return result
