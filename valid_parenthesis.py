754b66f49aa0ba1ceaa214f7d6695ee83c611b0a
vinayagamurthi47bcs28-commits <vinayagamurthi47_bcs28@mepcoeng.ac.in>
2026-08-25 16:12:54 +0530

    Update valid_parenthesis.py

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -2,7 +2,6 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack=[]
         mapping = {")": "(", "}": "{", "]": "["}
-
         for char in s:
             if char in mapping:
                 top_element = stack.pop() if stack else '#'
@@ -10,8 +9,4 @@ class Solution:
                     return False
             else:
                 stack.append(char)
-
-
-
-
         return not stack
