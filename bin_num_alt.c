286d79ab461882cd2dce62f87172fab621344561
vinayagamurthi47bcs28-commits <vinayagamurthi47_bcs28@mepcoeng.ac.in>
2026-08-25 16:05:08 +0530

    Update bin_num_alt.c

--- bin_num_alt.c
+++ bin_num_alt.c
@@ -1,9 +1,5 @@
 #include <stdbool.h>
-
 bool hasAlternatingBits(int n) {
-    // Shift n to the right by 1 to align opposite bits
-    unsigned int x = n ^ (n >> 1);
-    
-    // Check if x is of the form 111...111 (all bits set to 1)
+        unsigned int x = n ^ (n >> 1);
     return (x & (x + 1)) == 0;
 }
