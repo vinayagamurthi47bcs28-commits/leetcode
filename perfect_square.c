a9fc777be295f955ebfd1acaa0a29072be638c4e
vinayagamurthi47bcs28-commits <vinayagamurthi47_bcs28@mepcoeng.ac.in>
2026-08-25 16:03:21 +0530

    Update perfect_square.c

--- perfect_square.c
+++ perfect_square.c
@@ -1,23 +1,14 @@
 class Solution {
     public int numSquares(int n) {
-        // dp[i] will store the least number of perfect square numbers that sum to i
         int[] dp = new int[n + 1];
-        
-        // Base case: dp[0] = 0 (0 requires 0 squares)
         dp[0] = 0;
-        
-        // Fill the dp array from 1 to n
         for (int i = 1; i <= n; i++) {
             int minSquares = Integer.MAX_VALUE;
-            
-            // Try subtracting every perfect square j*j less than or equal to i
             for (int j = 1; j * j <= i; j++) {
                 minSquares = Math.min(minSquares, dp[i - j * j] + 1);
             }
-            
             dp[i] = minSquares;
         }
-        
         return dp[n];
     }
 }
