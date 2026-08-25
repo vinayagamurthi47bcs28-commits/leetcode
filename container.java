63ffb38b3d217e9fb43518d08a6072d05516b05c
vinayagamurthi47bcs28-commits <vinayagamurthi47_bcs28@mepcoeng.ac.in>
2026-08-25 16:08:17 +0530

    Update container.java

--- container.java
+++ container.java
@@ -8,17 +8,13 @@ class Solution {
             int width = right - left;
             int currentHeight = Math.min(height[left], height[right]);
             int currentWater = width * currentHeight;
-            
-            // FIX: Removed the 'if' keyword and correctly assigned the maximum value
             maxWater = Math.max(maxWater, currentWater);
-
             if (height[left] < height[right]) {
                 left++;
             } else {
                 right--;
             }
         }
-
         return maxWater;
     }
 }
